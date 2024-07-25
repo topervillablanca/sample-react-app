796e7c401504434d391bbaec21ccef65fd0aa654
topervillablanca <topervillablanca@gmail.com>
2024-07-25 20:28:18 +0800

    API Gateway update

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -2,15 +2,27 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Function, InlineCode, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
+import { LambdaIntegration, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class LambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
         super(scope, id, props);
-        new Function(this, 'LambdaFunction', {
+        const lambdaFunc = new NodejsFunction(this, 'LambdaFunction', {
+            entry: 'lib/lambda/handler.ts',
             runtime: Runtime.NODEJS_16_X,
-            handler: 'handler.handler',
+            handler: 'handler',
             code: Code.fromAsset(path.join(__dirname, 'lambda')),
             environment: { "stageName": stageName}
         })
+
+        const gateway = new LambdaRestApi(this, 'RESTApiSample', {
+            handler: lambdaFunc,
+            proxy: false,
+            restApiName: "trialAPI"
+        })
+
+        const helloAPI = gateway.root.addResource("hello")
+        helloAPI.addMethod("GET", new LambdaIntegration(lambdaFunc))
     }
 }
